0ef665dc866d07faffaf3f897b76c35dabb0401c
Nuke28 <naturalnatural774@gmail.com>
2025-07-08 09:56:47 +0600

    Update Main.java

--- src/main/java/com/autobots/Main.java
+++ src/main/java/com/autobots/Main.java
@@ -4,6 +4,7 @@ package com.autobots;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     //Hallo World!!
+    // Main World!
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
