7dd85d83f77beb5184476eafe6be9e118cb93201
Nuke28 <naturalnatural774@gmail.com>
2025-07-08 10:05:05 +0600

    Update Main.java

--- src/main/java/com/autobots/Main.java
+++ src/main/java/com/autobots/Main.java
@@ -5,6 +5,7 @@ package com.autobots;
 public class Main {
     //Hallo World!!
     // Main World!
+    //Git pool practice!
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
